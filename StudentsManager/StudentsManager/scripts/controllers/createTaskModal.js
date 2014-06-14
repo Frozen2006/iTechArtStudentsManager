0236d14a95d463193c852841d1886a05d0eadc08
Ilya.Starostin <bkmz2005@yandex.ru>
2014-06-15 00:01:48 +0300

    Task managment ALMOST implemented

---
+++ StudentsManager/StudentsManager/scripts/controllers/createTaskModal.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+
+iTechArtStudentsManagerApp.controller('CreateTaskModalController', ['$scope', 'hubProvider', '$modalInstance', function ($scope, hubProvider, $modalInstance) {
+
+    $scope.currentTask = {
+        title: '',
+        tag: '',
+        content: '',
+        complexLevel: ''
+    };
+
+    $scope.isErrorVisible = false;
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss();
+    };
+
+    $scope.ok = function () {
+        if (!$scope.currentTask.title || !$scope.currentTask.tag || !$scope.currentTask.content 
+            || !$scope.currentTask.complexLevel.match(/^[0-9]+$/) ) {
+            $scope.isErrorVisible = true;
+            return;
+        };
+        $modalInstance.close({
+            Title: $scope.currentTask.title,
+            Tag: $scope.currentTask.tag,
+            Content: $scope.currentTask.content,
+            ComplexLevel: $scope.currentTask.complexLevel
+        });
+    };
+
+}]);
