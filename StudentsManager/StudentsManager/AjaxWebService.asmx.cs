98fe4466cf9c2c56cc2ca3a673d856102118332f
Ilya.Starostin <bkmz2005@yandex.ru>
2014-06-14 12:27:24 +0300

    Initial commit. Structure was implemented

---
+++ StudentsManager/StudentsManager/AjaxWebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace StudentsManager
+{
+    /// <summary>
+    /// Summary description for AjaxWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AjaxWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
