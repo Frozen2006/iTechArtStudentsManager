b633e36ec6555d3c9922733d118c1e8440202e6e
anton-matseichyk <anton.matseichyk@itechart-group.com>
2014-06-14 18:31:13 +0300

    Angular login raw

--- StudentsManager/StudentsManager/AjaxWebService.asmx.cs
+++ StudentsManager/StudentsManager/AjaxWebService.asmx.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Services;
 using System.Web.Services;
+
+
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using DAL.Entities;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace StudentsManager
 {
     /// <summary>
     /// Summary description for AjaxWebService
     /// </summary>
+    [ScriptService]
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
@@ -21,6 +31,53 @@ namespace StudentsManager
         public string HelloWorld()
         {
             return "Hello World";
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        async public Task<string> Login(string username, string passwordHash)
+        {
+            var responseJson = "{\"authenticationResult\":\"{{authenticationResult}}\"," +
+                                "\"message\":\"{{message}}\"}";
+
+            bool authenticationResult = true;
+            string message = "";
+
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            var userByName = userManager.FindByName(username);
+            IdentityUser userByPassword = null;
+            if (userByName != null)
+            {
+                userByPassword = userManager.Find(username, passwordHash);
+            }
+
+
+            if (userByPassword != null)
+            {
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                authenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+                var identity = await userManager.CreateIdentityAsync(userByPassword, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = true }, identity);
+            }
+            else
+            {
+
+                message = "Invalid email or password";
+                authenticationResult = false;
+            }
+
+           
+
+            return responseJson
+                    .Replace("{{authenticationResult}}", authenticationResult.ToString())
+                    .Replace("{{message}}", message);
+        }
+
+        private async Task SignInAsync(ApplicationUser user, bool isPersistent)
+        {
+            
         }
     }
 }
--- StudentsManager/StudentsManager/scripts/controllers/login.js
+++ StudentsManager/StudentsManager/scripts/controllers/login.js
@@ -1,4 +1,30 @@
 ﻿iTechArtStudentsManagerApp.controller('LoginCtrl', ['$scope', function ($scope) {
     $scope.login = '';
     $scope.pass = '';
+
+    $scope.authenticate = function () {
+        console.log('Auth with' + $scope.login + ' ' + $scope.pass);
+        var url = "AjaxWebService.asmx";
+        $.ajax({
+            type: "POST",
+            url: url + "/Login",
+            data: "{'username':'" + $scope.login + "', 'passwordHash':'" + $scope.pass + "'}",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: OnSuccessCall,
+            error: OnErrorCall
+        });
+
+        function OnSuccessCall(responseJson) {
+            var response = JSON.parse(responseJson.d);
+            alert(response.authenticationResult);
+        }
+
+
+        function OnErrorCall(response) {
+            alert(response.status + " " + response.statusText);
+        }
+    }
+
+
 }]);
