01762007c21ce67ef93bd59a8e95093491e33839
anton-matseichyk <anton.matseichyk@itechart-group.com>
2014-06-14 15:35:21 +0300

    Added DAL with identity

---
+++ StudentsManager/DAL/StudentsManagerDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+using DAL.Entities;
+
+namespace DAL
+{
+    public class StudentsManagerDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public virtual IDbSet<Comment> Comments { get; set; }
+        public virtual IDbSet<File> Files { get; set; }
+        public virtual IDbSet<Group> Groups { get; set; }
+        public virtual IDbSet<Lection> Lections { get; set; }
+        public virtual IDbSet<LectionGroup> LectionGroups { get; set; }
+        public virtual IDbSet<Notification> Notifications { get; set; }
+        public virtual IDbSet<NotificationReceivers> NotificationReceivers { get; set; }
+        public virtual IDbSet<NotificationSenders> NotificationSenders { get; set; }
+        public virtual IDbSet<DAL.Entities.Task> Tasks { get; set; }
+        public virtual IDbSet<TaskStudent> TaskStudents { get; set; }
+        public virtual IDbSet<UserGroup> UserGroups { get; set; }
+
+        public StudentsManagerDbContext()
+            : base("StudentsManagerDb")
+        {
+            Database.SetInitializer<StudentsManagerDbContext>(new CreateDatabaseIfNotExists< StudentsManagerDbContext>() );
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id);
+            modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
+
+            modelBuilder.Entity<NotificationSenders>()
+                .HasOptional(f => f.Notification)
+                .WithRequired(s => s.NotificationSender);
+        }
+
+
+    }
+}
---
+++ StudentsManager/StudentsManager/Security/Identity/ApplicationUserStore.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Threading.Tasks;
+
+using Microsoft.AspNet.Identity.EntityFramework;
+
+using DAL;
+using DAL.Entities;
+
+namespace StudentsManager.Security.Identity
+{
+    public class ApplicationUserStore<TUser> : UserStore<TUser> where TUser : ApplicationUser
+    {
+        public ApplicationUserStore(StudentsManagerDbContext dbContext)
+            : base(dbContext)
+        {
+        }
+
+        public virtual Task<TUser> FindByEmailCustomAsync(string email)
+        {
+
+
+            return System.Threading.Tasks.Task.FromResult<TUser>(
+                Queryable.FirstOrDefault<TUser>(
+                    Queryable.Where<TUser>(
+                        Context.Set<TUser>(),
+                        (Expression<Func<TUser, bool>>)(u => u.Email.ToUpper() == email.ToUpper())
+                    )
+                )
+            );
+        }
+    }
+}
